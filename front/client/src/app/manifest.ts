689b158ceae16e0ead8541fdfb3100b7aa54a4d8
Artem Zhyto <artemzhr11@gmail.com>
2026-03-08 17:13:04 +0200

    chore: Added QuCore Dynamic immutable logos and SEO files

---
+++ front/client/src/app/manifest.ts
@@ -0,0 +1,45 @@
+// Modules
+import { MetadataRoute } from "next"
+
+// Config
+import { siteConfig } from "@config/metadata"
+
+/**
+ * PWA Manifest for QuCore Dynamic modules.
+ *
+ * Change only the values below to customize per module/site:
+ * - name
+ * - short_name
+ * - description
+ */
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: siteConfig.title.default,
+    short_name: "QuCore",
+    description: siteConfig.description,
+    start_url: "/",
+    display: "standalone",
+    background_color: "#000000",
+    theme_color: "#000000",
+    icons: [
+      {
+        src: "/icon.png",
+        sizes: "70x70",
+        type: "image/png",
+      },
+      {
+        src: "/icon-192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "/icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any",
+      },
+    ],
+  }
+}
---
+++ full/client/src/config/schema.ts
@@ -0,0 +1,32 @@
+// Modules
+import { siteConfig } from "./metadata"
+
+/**
+ * Immutable JSON-LD template for QuCore Dynamic.
+ * Change only: name, description and url (metadataBase).
+ */
+
+export const jsonLd = {
+  "@context": "https://schema.org",
+  "@type": "WebApplication",
+  name: "QuCore Dynamic",
+  description: siteConfig.description,
+  url: siteConfig.metadataBase.toString(),
+  logo: {
+    "@type": "ImageObject",
+    url: `${siteConfig.metadataBase.origin}/icon-512.png`,
+    width: 512,
+    height: 512,
+  },
+  applicationCategory: "BusinessApplication",
+  operatingSystem: "Windows, macOS, Android, iOS",
+  author: {
+    "@type": "Person",
+    name: siteConfig.twitter.creator,
+  },
+  inLanguage: "en-US",
+  address: {
+    "@type": "PostalAddress",
+    addressCountry: "UA",
+  },
+}
