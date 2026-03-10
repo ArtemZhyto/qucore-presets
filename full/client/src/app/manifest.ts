050c2048e65bc23a01e6e446a80c935bab4f5cc6
Artem Zhyto <artemzhr11@gmail.com>
2026-03-10 11:06:46 +0200

    fix: fixed icons in manifest.ts for PWA

--- full/client/src/app/manifest.ts
+++ full/client/src/app/manifest.ts
@@ -1,8 +1,8 @@
 // Modules
-import { MetadataRoute } from "next"
+import { MetadataRoute } from 'next'
 
 // Config
-import { siteConfig } from "@config/metadata"
+import { siteConfig } from '@config/metadata'
 
 /**
  * PWA Manifest for QuCore Dynamic modules.
@@ -15,29 +15,24 @@ import { siteConfig } from "@config/metadata"
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: siteConfig.title.default,
-    short_name: "QuCore",
+    short_name: 'QC Auth',
     description: siteConfig.description,
-    start_url: "/",
-    display: "standalone",
-    background_color: "#000000",
-    theme_color: "#000000",
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#171219',
+    theme_color: '#171219',
     icons: [
       {
-        src: "/icon.png",
-        sizes: "70x70",
-        type: "image/png",
+        src: '/icon-192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable',
       },
       {
-        src: "/icon-192.png",
-        sizes: "192x192",
-        type: "image/png",
-        purpose: "maskable",
-      },
-      {
-        src: "/icon-512.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "any",
+        src: '/icon-512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any',
       },
     ],
   }
