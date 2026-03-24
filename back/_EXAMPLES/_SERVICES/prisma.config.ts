aae8df5a8a87566912190c3415950efdf93f449a
Artem Zhyto <artemzhr11@gmail.com>
2026-03-24 14:53:30 +0200

    feat: hot reload for Docker has been added

--- back/_EXAMPLES/_SERVICES/prisma.config.ts
+++ back/_EXAMPLES/_SERVICES/prisma.config.ts
@@ -1,6 +1,12 @@
 // Modules
-import "dotenv/config"
-import { defineConfig } from "prisma/config"
+import 'dotenv/config'
+import { defineConfig } from 'prisma/config'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+dotenv.config({ path: path.resolve(__dirname, '../../docker/.env') })
 
 export default defineConfig({
   schema: `prisma/schema.prisma`,
