27051768c7b8256bead17a3d34ff873e04c7ea30
Artem Zhyto <artemzhr11@gmail.com>
2026-04-05 21:32:00 +0300

    update: fixed process.env.SERVICE_PORT in config.ts

--- back/_EXAMPLES/_SERVICES/src/config.ts
+++ back/_EXAMPLES/_SERVICES/src/config.ts
@@ -6,7 +6,7 @@ dotenv.config({ path: path.resolve(process.cwd(), '../../docker/.env') })
 
 const isProd = process.env.MODE === 'prod'
 
-export const __PORT = process.env.AUTH_SERVICE
+export const __PORT = process.env.SERVICE_PORT
 export const cookieOptions = {
   httpOnly: true,
   sameSite: (isProd ? 'strict' : 'lax') as 'strict' | 'lax',
